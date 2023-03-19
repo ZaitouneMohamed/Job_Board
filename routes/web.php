75f4cd449c83b113695c89f8e2eed5aa1dca1d91
mohamed zaitoune <mohamedztn37@gmail.com>
2023-03-19 22:59:14 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\fournisseur\HomeController as FournisseurHomeController;
+use App\Http\Controllers\job\HomeController as JobHomeController;
+use App\Http\Controllers\LangController;
+use App\Http\Controllers\user\homeController as UserHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +23,11 @@ Route::get('/', function () {
     return view('job.index');
 });
 
-Route::middleware(['auth','role:admin'])->name('admin.')->prefix('admin')->group(function() {
+Route::get('lang', [LangController::class,'change'])->name('changeLang');
+Route::get('job_detail/{id}', [JobHomeController::class , 'job_details'] )->name('job_detail');
+
+
+Route::middleware(['auth','role:admin','active'])->name('admin.')->prefix('admin')->group(function() {
     Route::get('/', function () {
         return view('admin.index');
     });
@@ -35,15 +43,30 @@ Route::middleware(['auth','role:admin'])->name('admin.')->prefix('admin')->group
     });
 });
 
-Route::middleware(['auth','role:fournisseur'])->name('fournisseur.')->prefix('is-admin')->group(function() {
+Route::middleware(['auth','role:fournisseur','active'])->name('fournisseur.')->prefix('is-admin')->group(function() {
     Route::get('/', function () {
         return view('fournisseur.index');
     });
+    Route::get('/company', function () {
+        return view('fournisseur.company');
+    })->name('company');
+    Route::get('/mes-annonces', function () {
+        return view('fournisseur.annonces.index');
+    })->name('annonces.index');
+    Route::get('/user_on_annonce/{id}' , [FournisseurHomeController::class , 'users_pending_on_job' ])->name('user_on_annonce');
+
 });
 
-Route::middleware(['auth','role:user'])->name('user.')->prefix('user')->group(function() {
+Route::middleware(['auth','role:user','active'])->name('user.')->prefix('user')->group(function() {
     Route::get('/', function () {
         return view('user.index');
+    });
+    Route::controller(UserHomeController::class)->group(function() {
+        Route::get('profile' , 'profile')->name('profile');
+        Route::get('pending_jobs' , 'my_pending_jobs')->name('pending_jobs');
+        Route::get('edit_profile' , 'edit_profile')->name('profile.edit');
+        Route::post('update_profile' , 'update_profile')->name('profile.update');
+        Route::get('apply_job/{user_id}/{job_id}/apply_for_job' , 'apply_job')->name('apply_job');
     });
 });
 
