da56142a3b4cda298eb8a308017b18fef8f330d1
mohamed zaitoune <mohamedztn37@gmail.com>
2023-03-20 23:09:20 +0000

    check if user like any annonce and like unlike annonce buttons

--- resources/views/livewire/job/jobslist.blade.php
+++ resources/views/livewire/job/jobslist.blade.php
@@ -1,4 +1,4 @@
-<div class="container-xxl py-5" >
+<div class="container-xxl py-5">
     <div class="container">
         <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Job Listing</h1>
         <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
@@ -14,28 +14,43 @@
                     </a>
                 </li>
             </ul>
-            <div class="tab-content" >
-                <div id="tab-2" class="tab-pane fade show p-0 active" >
+            <div class="tab-content">
+                <div id="tab-2" class="tab-pane fade show p-0 active">
                     @foreach ($full_time_jobs as $item)
                         <div class="job-item p-4 mb-4">
                             <div class="row g-4">
                                 <div class="col-sm-12 col-md-8 d-flex align-items-center">
-                                    <img class="flex-shrink-0 img-fluid border rounded" src="dashboards/landing/img/com-logo-5.jpg" alt="" style="width: 80px; height: 80px;">
+                                    <img class="flex-shrink-0 img-fluid border rounded"
+                                        src="dashboards/landing/img/com-logo-5.jpg" alt=""
+                                        style="width: 80px; height: 80px;">
                                     <div class="text-start ps-4">
-                                        <h5 class="mb-3">{{$item->title}}</h5>
-                                        <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{Str::limit($item->company->adresse, 20, '...') }}</span>
-                                        <span class="text-truncate me-3"><i class="far fa-clock text-primary me-2"></i>{{$item->nature}}</span>
-                                        <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>{{$item->salary}}</span>
+                                        <h5 class="mb-3">{{ $item->title }}</h5>
+                                        <span class="text-truncate me-3"><i
+                                                class="fa fa-map-marker-alt text-primary me-2"></i>{{ Str::limit($item->company->adresse, 20, '...') }}</span>
+                                        <span class="text-truncate me-3"><i
+                                                class="far fa-clock text-primary me-2"></i>{{ $item->nature }}</span>
+                                        <span class="text-truncate me-0"><i
+                                                class="far fa-money-bill-alt text-primary me-2"></i>{{ $item->salary }}</span>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                                <div
+                                    class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
                                     <div class="d-flex mb-3">
                                         @auth
-                                            <button class="btn btn-light btn-square me-3"><i class="far fa-heart text-primary"></i></button>
+                                            @if (auth()->user()->fav_job->contains($item->id))
+                                                <button class="btn btn-light btn-square me-3" wire:click="remove_from_favorites({{$item->id}},{{auth()->user()->id}})"><i
+                                                        class="far fa-heart text-danger"></i></button>
+                                            @else
+                                                <button class="btn btn-light btn-square me-3" wire:click="add_to_favorites({{$item->id}},{{auth()->user()->id}})"><i
+                                                        class="far fa-heart text-primary"></i></button>
+                                            @endif
                                         @endauth
-                                        <a class="btn btn-primary" href="{{route('job_detail',$item->id)}}">view job</a>
+                                        <a class="btn btn-primary" href="{{ route('job_detail', $item->id) }}">view
+                                            job</a>
                                     </div>
-                                    <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Datee Line: {{$item->created_at}}</small>
+                                    <small class="text-truncate"><i
+                                            class="far fa-calendar-alt text-primary me-2"></i>Datee Line:
+                                        {{ $item->created_at }}</small>
                                 </div>
                             </div>
                         </div>
@@ -47,22 +62,32 @@
                         <div class="job-item p-4 mb-4">
                             <div class="row g-4">
                                 <div class="col-sm-12 col-md-8 d-flex align-items-center">
-                                    <img class="flex-shrink-0 img-fluid border rounded" src="dashboards/landing/img/com-logo-5.jpg" alt="" style="width: 80px; height: 80px;">
+                                    <img class="flex-shrink-0 img-fluid border rounded"
+                                        src="dashboards/landing/img/com-logo-5.jpg" alt=""
+                                        style="width: 80px; height: 80px;">
                                     <div class="text-start ps-4">
-                                        <h5 class="mb-3">{{$item->title}}</h5>
-                                        <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{Str::limit($item->company->adresse, 20, '...') }}</span>
-                                        <span class="text-truncate me-3"><i class="far fa-clock text-primary me-2"></i>{{$item->nature}}</span>
-                                        <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>${{$item->salary}}</span>
+                                        <h5 class="mb-3">{{ $item->title }}</h5>
+                                        <span class="text-truncate me-3"><i
+                                                class="fa fa-map-marker-alt text-primary me-2"></i>{{ Str::limit($item->company->adresse, 20, '...') }}</span>
+                                        <span class="text-truncate me-3"><i
+                                                class="far fa-clock text-primary me-2"></i>{{ $item->nature }}</span>
+                                        <span class="text-truncate me-0"><i
+                                                class="far fa-money-bill-alt text-primary me-2"></i>${{ $item->salary }}</span>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                                <div
+                                    class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
                                     <div class="d-flex mb-3">
                                         @auth
-                                            <button class="btn btn-light btn-square me-3"><i class="far fa-heart text-primary"></i></button>
+                                            <button class="btn btn-light btn-square me-3"><i
+                                                    class="far fa-heart text-primary"></i></button>
                                         @endauth
-                                        <a class="btn btn-primary" href="{{route('job_detail',$item->id)}}">view job</a>
+                                        <a class="btn btn-primary" href="{{ route('job_detail', $item->id) }}">view
+                                            job</a>
                                     </div>
-                                    <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Datee Line: {{$item->created_at}}</small>
+                                    <small class="text-truncate"><i
+                                            class="far fa-calendar-alt text-primary me-2"></i>Datee Line:
+                                        {{ $item->created_at }}</small>
                                 </div>
                             </div>
                         </div>
