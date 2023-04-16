1f4742f8d07ec2e836c4f857f6728347917e81c4
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-04-16 13:28:16 +0000

    small design changes

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,21 +1,74 @@
-@extends("admin.master.master")
+@extends('admin.master.master')
 
-@section("content")
-<h1>update info</h1>
-    <form action="{{route('update_profile')}}" method="post">
-        @csrf
-        @method("post")
-        <input type="text" name="name" value="{{auth()->user()->name}}" id="" ><br>
-        <input type="email" name="email" value="{{auth()->user()->email}}" id=""><br>
-        <button type="submit" class="btn btn-primary">submit</button>
-    </form>
-    <h1>update password</h1>
-    <form action="{{route('update_password')}}" method="post">
-        @csrf
-        @method("post")
-        <input type="password" name="old_password" id=""><br>
-        <input type="password" name="password1" id=""><br>
-        <input type="password" name="password2" id=""><br>
-        <button type="submit" class="btn btn-primary">submit</button>
-    </form>
+@section('content')
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">change password</a></li>
+                </ul>
+            </div><!-- /.card-header -->
+            <div class="card-body">
+                <div class="tab-content">
+                    <div class="active tab-pane" id="activity">
+                        <!-- Post -->
+                        <form class="form-horizontal" action="{{route('update_profile')}}" method="post">
+                            @csrf
+                            @method("post")
+                            <div class="form-group row">
+                                <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="name" value="{{auth()->user()->name}}" id="inputName" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="email" name="email" value="{{auth()->user()->email}}" class="form-control" id="inputEmail" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="offset-sm-2 col-sm-10">
+                                    <button type="submit" class="btn btn-danger">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                        <!-- /.post -->
+                    </div>
+
+                    <div class="tab-pane" id="settings">
+                        <form class="form-horizontal">
+                            <div class="form-group row">
+                                <label for="inputName" class="col-sm-2 col-form-label">old password</label>
+                                <div class="col-sm-10">
+                                    <input type="password" name="old_password" class="form-control" id="inputName" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail" class="col-sm-2 col-form-label">new password</label>
+                                <div class="col-sm-10">
+                                    <input type="email" name="password1" class="form-control" id="inputEmail" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail" class="col-sm-2 col-form-label">repeat password</label>
+                                <div class="col-sm-10">
+                                    <input type="password" name="password2" class="form-control" id="inputEmail" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="offset-sm-2 col-sm-10">
+                                    <button type="submit" class="btn btn-danger">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+            </div><!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
 @endsection
